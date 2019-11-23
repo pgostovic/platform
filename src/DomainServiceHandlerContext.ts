29bd2d31319e815b297c6970af46af0f2cd83669
Patrick Gostovic <pg@patricks-mbp.lan>
2019-11-23 17:09:39 -0500

    Send notification to current account by default.

--- src/DomainServiceHandlerContext.ts
+++ src/DomainServiceHandlerContext.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { MessageConnection, Value } from '@phnq/message';
+import { ModelId, search } from '@phnq/model';
 
+import { AuthApi } from './domains/auth/AuthApi';
+import Session from './domains/auth/model/Session';
 import { DomainServiceApi, DomainServiceMessage } from './types';
 
 export default class DomainServiceHandlerContext {
@@ -31,15 +34,39 @@ export default class DomainServiceHandlerContext {
     return this.connectionId;
   }
 
-  public notify(type: string, info: Value, connectionIds?: string[]): void {
-    (connectionIds || [this.connectionId]).forEach(connectionId => {
-      const message: DomainServiceMessage = {
-        type: `${this.domain}.${type}`,
-        info,
-        connectionId,
-        origin: '',
-      };
-      this.apiConnection.send(message);
-    });
+  /**
+   * Send a push notification. Recipient accounts may be specified, otherwise the current account
+   * is the recipient.
+   *
+   * @param type Notification message type
+   * @param info Notification message data
+   * @param recipientAccountIds (optional) account ids of the repients
+   */
+  public async notify(type: string, info: Value, recipientAccountIds?: ModelId[]): Promise<void> {
+    const { auth } = (this as unknown) as { auth: AuthApi };
+
+    let accountIds = recipientAccountIds;
+    if (!accountIds) {
+      const session = await search(Session, { auxId: this.connectionId }).first();
+      if (!session) {
+        throw new Error(`Could not find session by auxId: ${this.connectionId}`);
+      }
+      accountIds = [session.accountId];
+    }
+
+    for await (const accountId of accountIds) {
+      const connectionIds = await auth.getActiveConnectionIds({ accountId });
+
+      await Promise.all(
+        connectionIds.map(connectionId =>
+          this.apiConnection.send({
+            type: `${this.domain}.${type}`,
+            info,
+            connectionId,
+            origin: '',
+          }),
+        ),
+      );
+    }
   }
 }
