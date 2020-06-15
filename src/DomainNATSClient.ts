ef31c381f2ecc174cadd1cc6e656dc28ebe974bc
Patrick Gostovic <pg@patricks-mbp.lan>
2020-06-15 10:16:15 -0400

    Broadcast wildcard

--- src/DomainNATSClient.ts
+++ src/DomainNATSClient.ts
@@ -28,7 +28,7 @@ export default class DomainNATSClient extends DomainClient {
   protected async createMessageClient(): Promise<MessageConnection<ServiceMessage>> {
     const broadcastType = process.env.MESSAGE_BROADCAST_TYPE || 'broadcast';
     const natsTransport = await NATSTransport.create(this.natsConfig, {
-      subscriptions: [ORIGIN, `${broadcastType}.*`],
+      subscriptions: [ORIGIN, `${broadcastType}.*.*`],
       publishSubject: (message: Message): string => (message.p as DomainServiceMessage).type as string,
     });
     const signSalt = process.env.MESSAGE_SIGN_SALT;
